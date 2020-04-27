e50e47d99e6985c138ab0e2563c8fb60221efb60
ivanberry <tabvim.dev@gmail.com>
2020-04-27 09:00:02 +0800

    add route

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 import { GlobalStyle } from './style';
 import { IconStyle } from './asserts/iconfont/iconfont';
+import { renderRoutes } from 'react-router-config';
+import routes from './routes';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
-    return (
-        <div className="App">
-            <GlobalStyle></GlobalStyle>
-            <IconStyle></IconStyle>
-            <i className="iconfont">&#xe62b;</i>
-        </div>
-    );
+	return (
+		<HashRouter>
+			<GlobalStyle></GlobalStyle>
+			<IconStyle></IconStyle>
+			{renderRoutes(routes)}
+		</HashRouter>
+	);
 }
 
 export default App;
