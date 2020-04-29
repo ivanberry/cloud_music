f84dc8df5da876fbe86c527ca63c09e036d05e09
ivanberry <tabvim.dev@gmail.com>
2020-04-30 06:31:59 +0800

    add singer list api

--- src/api/request.js
+++ src/api/request.js
@@ -6,10 +6,28 @@
 
 import { axiosInstance } from "./config";
 
+/**
+ * 获取banner图列表
+ * @return {Promise<AxiosResponse<any>>}
+ */
 export const getBannerRequest = () => {
   return axiosInstance.get("/banner");
 };
 
+/**
+ * 获取歌单推荐列表
+ * @return {Promise<AxiosResponse<any>>}
+ */
 export const getRecommendListRequest = () => {
   return axiosInstance.get("/personalized");
+};
+
+export const getHotSingerListRequest = count => {
+  return axiosInstance.get(`/top/artists?offset=${count}`);
+};
+
+export const getSingerListRequest = (category, alpha, count) => {
+  return axiosInstance.get(
+    `/artist/list?cat=${category}&initial=${alpha.toLowerCase()}&offset=${count}`
+  );
 };
